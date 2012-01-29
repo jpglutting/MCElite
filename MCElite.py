92696c0ccd3587a23ebdff021e4e54923f938e86
JP Glutting <jpglutting@gmail.com>
2012-01-29 23:44:54 +0100

    Code cleaned and working ith QGIS 1.8-Trunk

--- MCElite.py
+++ MCElite.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 
 """
-/***************************************************************************
+***************************************************************************
 MCElite
-A QGIS plugin
 Multiple Criteria Analysis tool
-                                                         -------------------
-begin                                : 2010-08-27 
-copyright                        : (C) 2010 by JP Glutting
-email                                : jpglutting@gmail.com 
- ***************************************************************************/
+                             -------------------
+begin                : 2010-08-27 
+copyright            : (C) 2010 by JP Glutting
+email                : jpglutting@gmail.com 
+***************************************************************************
 
-/***************************************************************************
- *                                                                                                                                                 *
- *     This program is free software; you can redistribute it and/or modify    *
- *     it under the terms of the GNU General Public License as published by    *
- *     the Free Software Foundation; either version 2 of the License, or         *
- *     (at your option) any later version.                                                                     *
- *                                                                                                                                                 *
- ***************************************************************************/
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+
 """
 # Import the PyQt and QGIS libraries
 from PyQt4.QtCore import * 
--- MCEliteDialog.py
+++ MCEliteDialog.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 
 """
-/***************************************************************************
-MCEliteDialog
-A QGIS plugin
-Multiple Criteria Analysis decision support tool
+***************************************************************************
+MCElite
+Multiple Criteria Analysis tool
                              -------------------
 begin                : 2010-08-27 
 copyright            : (C) 2010 by JP Glutting
 email                : jpglutting@gmail.com 
- ***************************************************************************/
+***************************************************************************
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+
 """
 import os
 import string
